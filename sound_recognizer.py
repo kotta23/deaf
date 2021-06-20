3590900bd58e39e30c406b46583325e8ac8a19ea
kota23 <kotta.1115@gmail.com>
2021-06-20 21:02:05 +0100

    t1

--- sound_recognizer.py
+++ sound_recognizer.py
@@ -6,7 +6,7 @@ recognizer = sr.Recognizer()
 def recognize_speech_from_mic():
     global recognizer
     print('espeak "please say something.."')
-    os.system('''arecord -f S16_LE -d 5 -r 44100 --device="hw:2,0"  test.wav''')
+    os.system('''arecord -f S16_LE -d 5 -r 44100 --device="hw:1,0"  test.wav''')
     with sr.WavFile("test.wav") as source:              # use "test.wav" as the audio source
         audio = recognizer.record(source) 
     print('espeak "decodeing"')
